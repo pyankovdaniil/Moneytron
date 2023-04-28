5425cb96c0ea23c503b6fb609119d5d2dc9ab075
Pyankov Daniil <pyankovdaniil5@gmail.com>
2023-04-28 18:38:34 +0300

    Added Authentication Microservice, MongoDB and Redis

---
+++ Authentication/src/main/java/pyankovdaniil/microservices/dto/tokens/TokensResponse.java
@@ -0,0 +1,15 @@
+package pyankovdaniil.microservices.dto.tokens;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokensResponse {
+    private String accessToken;
+    private String refreshToken;
+}
---
+++ Authentication/src/main/java/pyankovdaniil/microservices/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,53 @@
+package pyankovdaniil.microservices.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final String AUTH_HEADER_NAME = "Authorization";
+    private static final String AUTH_HEADER_START = "Bearer ";
+    private static final int JWT_START_POSITION = AUTH_HEADER_START.length();
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        String authenticationHeader = request.getHeader(AUTH_HEADER_NAME);
+        if (authenticationHeader == null || !authenticationHeader.startsWith(AUTH_HEADER_START)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String jwt = authenticationHeader.substring(JWT_START_POSITION);
+        Optional<String> email = jwtService.extractEmail(jwt);
+        if (email.isPresent() && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(email.get());
+            if (jwtService.isJwtValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails,
+                        null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
