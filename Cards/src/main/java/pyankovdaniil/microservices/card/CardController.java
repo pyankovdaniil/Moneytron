99b762fc918eb187709e2a2ea333fd41a08c0f9b
Pyankov Daniil <pyankovdaniil5@gmail.com>
2023-05-17 19:06:14 +0300

    Added /remove-card endpoint

--- Cards/src/main/java/pyankovdaniil/microservices/card/CardController.java
+++ Cards/src/main/java/pyankovdaniil/microservices/card/CardController.java
@@ -13,35 +13,43 @@ import pyankovdaniil.microservices.clients.authentication.AuthenticationClient;
 import pyankovdaniil.microservices.clients.authentication.dto.BearerToken;
 import pyankovdaniil.microservices.clients.authentication.dto.ExtractedEmail;
 import pyankovdaniil.microservices.dto.AddCardRequest;
-
-import java.math.BigDecimal;
+import pyankovdaniil.microservices.dto.RemoveCardRequest;
 
 @RestController
 @RequestMapping("/api/v1/cards")
 @Slf4j
 public record CardController(
         AuthenticationClient authenticationClient,
-        CardRepository cardRepository) {
+        CardsService cardsService) {
     private static final String AUTH_HEADER_NAME = "Authorization";
 
     @PostMapping("/add-card")
     public ResponseEntity<?> addCard(HttpServletRequest request, @RequestBody AddCardRequest addCardRequest) {
         String bearerTokenHeader = request.getHeader(AUTH_HEADER_NAME);
         BearerToken bearerToken = BearerToken.builder().bearerToken(bearerTokenHeader).build();
-
         ResponseEntity<ExtractedEmail> response = authenticationClient.extractEmail(bearerToken);
         if (response.getStatusCode().equals(HttpStatusCode.valueOf(HttpStatus.OK.value()))) {
             ExtractedEmail extractedEmail = response.getBody();
-            log.info("Got /add-card request with ownerEmail: {} and cardName: {}", extractedEmail.getEmail(), addCardRequest.getCardName());
-            Card cardToInsert = Card.builder()
-                    .ownerEmail(extractedEmail.getEmail())
-                    .cardName(addCardRequest.getCardName())
-                    .balance(BigDecimal.ZERO)
-                    .build();
-            cardRepository.save(cardToInsert);
-            return ResponseEntity.ok(addCardRequest.getCardName());
+            if (extractedEmail != null) {
+                return cardsService.addCard(extractedEmail, addCardRequest);
+            }
         }
 
-        return new ResponseEntity<>("...", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>("Invalid Bearer token or request body!", HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/remove-card")
+    public ResponseEntity<?> removeCard(HttpServletRequest request, @RequestBody RemoveCardRequest removeCardRequest) {
+        String bearerTokenHeader = request.getHeader(AUTH_HEADER_NAME);
+        BearerToken bearerToken = BearerToken.builder().bearerToken(bearerTokenHeader).build();
+        ResponseEntity<ExtractedEmail> response = authenticationClient.extractEmail(bearerToken);
+        if (response.getStatusCode().equals(HttpStatusCode.valueOf(HttpStatus.OK.value()))) {
+            ExtractedEmail extractedEmail = response.getBody();
+            if (extractedEmail != null) {
+                return cardsService.removeCard(extractedEmail, removeCardRequest);
+            }
+        }
+
+        return new ResponseEntity<>("Invalid Bearer token or request body!", HttpStatus.BAD_REQUEST);
     }
 }
